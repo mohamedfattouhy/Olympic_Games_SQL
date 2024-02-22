8e4969874aa238004eeef09a45bf52e0a524c105
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2024-02-22 22:04:41 +0100

    Add queries 6 to 10

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -31,3 +31,40 @@ order by games;
 select noc, count(distinct games) as cnt from athlete_events
 group by noc
 having cnt = (select count(distinct games) from athlete_events);
+
+
+------------- Query 6 -------------
+-- Identify the sport which was played in all summer olympics
+select sport, count(sport) as cnt from (
+    select distinct sport, Games from athlete_events
+    where season = "summer") as x
+group by sport
+having cnt = (select count(distinct Games) from athlete_events where season = "summer");
+
+
+------------- Query 7 -------------
+-- Which Sports were just played only once in the olympics ?
+select sport, count(distinct Games) as cnt, min(games) as games from athlete_events
+group by sport
+having cnt = 1;
+
+
+------------- Query 8 -------------
+-- Fetch the total no of sports played in each olympic games
+select games, count(distinct sport) as total_sports
+from athlete_events
+group by games
+order by games;
+
+
+------------- Query 9 -------------
+-- Fetch oldest athletes to win a gold medal
+select year, name, age, games from athlete_events
+where medal="Gold" and age is not null
+order by age desc
+limit 2;
+
+
+------------- Query 10 -------------
+-- Find the Ratio of male and female athletes participated in all olympic games
+select (select count(sex) from athlete_events where sex="M")/(select count(sex) from athlete_events where sex="F") as ratio_male_female
